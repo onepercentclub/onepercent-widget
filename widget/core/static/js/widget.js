42b613841117aba87920319aaf7690c4126cafbd
Jhon Majoor <jhon@onepercentclub.com>
2014-10-14 15:45:12 +0200

    added progress slider + dayleft

--- widget/core/static/js/widget.js
+++ widget/core/static/js/widget.js
@@ -44,6 +44,50 @@ function loadEmber() {
 
 function main(){
 	jQuery(document).ready(function($){
+		function progressbar() {
+			$('.slider-progress').each(function() {
+		        var width = 0,
+					sliderProgress = $(this);
+		        	targetAmount = sliderProgress.data('target-amount'),
+		        	donatedAmount = sliderProgress.data('donated-amount');
+
+	        	sliderProgress.css('width', '0px');
+		        if (parseInt(targetAmount) > 0) {
+		        	if(parseInt(donatedAmount) >= parseInt(targetAmount)){
+		        		width = 100;
+		        	} else {
+			            width = 100 * parseInt(donatedAmount) / parseInt(targetAmount);
+		        	}
+		            width += '%';
+		        }
+		        $(this).animate({'width': width}, 1000);
+			})
+		}
+
+		function daysToGo() {
+			$('.days-left').each(function() {
+				var daysLeft = $(this).data('days-left');
+				if (!daysLeft) {
+				return null;
+				}
+				var now = new Date(),
+					d = now.getDay(),
+					m = now.getMonth(),
+					y = now.getFullYear();
+				console.log(daysLeft);
+				$(this).find('strong').html('jhon');
+				//debugger;
+				//var microseconds = daysLeft.getTime() - now.getTime();
+				//return Math.ceil(microseconds / (1000 * 60 * 60 * 24));
+			});
+
+		}
+
+		setTimeout(function(){
+			progressbar();
+			daysToGo();
+		}, 500);
+
 
 		var el = $('div#widget-container');
 		var id = $(el).data('id');
@@ -55,7 +99,6 @@ function main(){
 		$.getJSON(jsonp_url, function(data){
 			$('#widget-container').html(data.html);
 		});
-
 	});
 }
 
